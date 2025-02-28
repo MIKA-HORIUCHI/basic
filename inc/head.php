b89d6fffd9c9df89da68cd0acf2c2b9d04a8fe88
MIKA-HORIUCHI <61669891+MIKA-HORIUCHI@users.noreply.github.com>
2025-02-28 18:51:50 +0900

    サイト準備

--- inc/head.php
+++ inc/head.php
@@ -1,20 +1,20 @@
 <meta charset="utf-8">
-<title><?php echo $title; ?><?php echo $line; ?>サイトタイトル</title>
+<title><?php echo $title; ?><?php echo $line; ?>AiSLE OFFICIAL WEBSITE</title>
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1">
-<meta name="description" content="サイトテキスト">
+<meta name="description" content="AiSLEのオフィシャルサイトです。">
 <meta name="copyright" content="&copy;B ZONE Inc.">
 
-<meta property="og:title" content="サイトタイトル">
-<meta property="og:site_name" content="サイトタイトル">
-<meta property="og:type" content="website">
-<meta property="og:description" content="サイトテキスト">
-<meta property="og:url" content="サイトURL">
-<meta property="og:image" content="サイトURL/assets/img/ogp.jpg">
+<meta property="og:title" content="AiSLE OFFICIAL WEBSITE">
+<meta property="og:site_name" content="AiSLE OFFICIAL WEBSITE">
+<meta property="og:type" content=" website">
+<meta property="og:description" content="AiSLEのオフィシャルサイトです。">
+<meta property="og:url" content="http://aisle.bzone.co.jp">
+<meta property="og:image" content="http://aisle.bzone.co.jp/assets/img/ogp.jpg">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@">
-<meta name="twitter:title" content="サイトタイトル">
-<meta name="twitter:description" content="サイトテキスト">
-<meta name="twitter:image" content="サイトURL/assets/img/ogp.jpg">
+<meta name="twitter:title" content="AiSLE OFFICIAL WEBSITE">
+<meta name="twitter:description" content="AiSLEのオフィシャルサイトです。">
+<meta name="twitter:image" content="http://aisle.bzone.co.jp/assets/img/ogp.jpg">
 
 <link rel="icon" href="/assets/img/favicon.ico" />
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $path; ?>/assets/img/apple-touch-icon.png">
@@ -28,4 +28,6 @@
 
 <script src="<?php echo $path; ?>assets/js/script.js?<?php echo date('Ymd-Hi'); ?>"></script>
 
+<!-- Font -->
+<link href="https://api.fontshare.com/v2/css?f[]=clash-display@1&display=swap" rel="stylesheet">
 <!-- Global site tag (gtag.js) - Google Analytics -->
